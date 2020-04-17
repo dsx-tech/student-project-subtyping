6aab41ebc313de6bfa094599ea2872c81c2a0d61
zhvkgj <zhvkgj@yandex.ru>
2020-04-17 19:58:46 +0500

    minor refactoring

--- subtyping-annotation-processor/src/main/java/annvisitor/AnnotationValueTypeTreeScanner.java
+++ subtyping-annotation-processor/src/main/java/annvisitor/AnnotationValueTypeTreeScanner.java
@@ -47,54 +47,6 @@ public class AnnotationValueTypeTreeScanner extends TreeScanner<String, String> 
         return AnnTreeScanHolder.HOLDER_INSTANCE;
     }
 
-    private ResultKind checkParamsMatching(List<? extends ExpressionTree> actParams,
-                                           ExecutableElement exec,
-                                           String p) {
-        List<? extends VariableElement> formParams = exec.getParameters();
-
-        if (actParams == null ^ formParams == null) {
-            return ResultKind.NULLITY;
-        }
-
-        if (actParams != null) {
-            if (actParams.size() != formParams.size()) {
-                return ResultKind.DIFFERENT_SIZE;
-            } else {    // lists of actual and formal parameters != null and have the same size
-                Iterator<? extends ExpressionTree> it1 = actParams.iterator();
-                Iterator<? extends VariableElement> it2 = formParams.iterator();
-
-                while (it1.hasNext() && it2.hasNext()) {
-                    ExpressionTree var1 = it1.next();
-                    VariableElement var2 = it2.next();
-                    String type1 = scan(var1, p);
-                    String type2 = Top.class.getName();
-
-                    Type ann2 = var2.getAnnotation(Type.class);
-                    TypeMirror value = null;
-
-                    if (ann2 != null) {
-                        try {
-                            ann2.value();
-                        } catch (MirroredTypeException mte) {
-                            value = mte.getTypeMirror();
-                        }
-                        if (value != null) {
-                            type2 = value.toString();
-                        }
-                    }
-                    if (type1 != null && !type1.equals(Top.class.getName()) && type2.equals(Top.class.getName())) {
-                        return ResultKind.NON_ANNOTATED_PARAM_WARNING;
-                    }
-                    if (!isSubtype(type1, type2, processingEnv)) {
-                        return ResultKind.TYPE_MISMATCH_PARAMS;
-                    }
-                }
-            }
-        }
-
-        return ResultKind.OK;
-    }
-
     @Override
     public String visitMethod(MethodTree node, String aVoid) {
         // node is a constructor
@@ -194,6 +146,54 @@ public class AnnotationValueTypeTreeScanner extends TreeScanner<String, String> 
         return type;
     }
 
+    private ResultKind checkParamsMatching(List<? extends ExpressionTree> actParams,
+                                           ExecutableElement exec,
+                                           String p) {
+        List<? extends VariableElement> formParams = exec.getParameters();
+
+        if (actParams == null ^ formParams == null) {
+            return ResultKind.NULLITY;
+        }
+
+        if (actParams != null) {
+            if (actParams.size() != formParams.size()) {
+                return ResultKind.DIFFERENT_SIZE;
+            } else {    // lists of actual and formal parameters != null and have the same size
+                Iterator<? extends ExpressionTree> it1 = actParams.iterator();
+                Iterator<? extends VariableElement> it2 = formParams.iterator();
+
+                while (it1.hasNext() && it2.hasNext()) {
+                    ExpressionTree var1 = it1.next();
+                    VariableElement var2 = it2.next();
+                    String type1 = scan(var1, p);
+                    String type2 = Top.class.getName();
+
+                    Type ann2 = var2.getAnnotation(Type.class);
+                    TypeMirror value = null;
+
+                    if (ann2 != null) {
+                        try {
+                            ann2.value();
+                        } catch (MirroredTypeException mte) {
+                            value = mte.getTypeMirror();
+                        }
+                        if (value != null) {
+                            type2 = value.toString();
+                        }
+                    }
+                    if (type1 != null && !type1.equals(Top.class.getName()) && type2.equals(Top.class.getName())) {
+                        return ResultKind.NON_ANNOTATED_PARAM_WARNING;
+                    }
+                    if (!isSubtype(type1, type2, processingEnv)) {
+                        return ResultKind.TYPE_MISMATCH_PARAMS;
+                    }
+                }
+            }
+        }
+
+        return ResultKind.OK;
+    }
+
     @Override
     public String visitMethodInvocation(MethodInvocationTree node, String aVoid) {
         ExecutableElement method = (ExecutableElement) mTrees.getElement(mTrees.getPath(cut, node));
@@ -241,51 +241,35 @@ public class AnnotationValueTypeTreeScanner extends TreeScanner<String, String> 
         return var;
     }
 
+    private void checkOperandsPermission(ExpressionTree operand, Tree.Kind operator, String type) {
+        PermissionPolicy permissionForExpr = isOperationAllow(operator, type, processingEnv);
+        switch (permissionForExpr) {
+            case FORBID:
+                printResultInfo(operand, ResultKind.WRONG_APPLY_OPERATOR, mTrees, cut);
+                break;
+            case ALLOW_WITH_WARNING:
+                printResultInfo(operand, ResultKind.APPLY_OPERATOR_WITH_WARNING, mTrees, cut);
+                break;
+            default:
+        }
+    }
+
     @Override
     public String visitCompoundAssignment(CompoundAssignmentTree node, String aVoid) {
         String var = scan(node.getVariable(), aVoid);
         String expr = scan(node.getExpression(), aVoid);
-        Tree.Kind op = node.getKind();
         if (!expr.equals(Top.class.getName()) && !isSubtype(expr, var, processingEnv)) {
             printResultInfo(node, ResultKind.INCORRECT_VARIABLE_TYPE, mTrees, cut);
         }
-        PermissionPolicy permissionForVar = isOperationAllow(op, var, processingEnv);
-        PermissionPolicy permissionForExpr = isOperationAllow(op, expr, processingEnv);
-        switch (permissionForExpr) {
-            case FORBID:
-                printResultInfo(node.getExpression(), ResultKind.WRONG_APPLY_OPERATOR, mTrees, cut);
-                break;
-            case ALLOW_WITH_WARNING:
-                printResultInfo(node.getExpression(), ResultKind.APPLY_OPERATOR_WITH_WARNING, mTrees, cut);
-                break;
-            default:
-        }
-        switch (permissionForVar) {
-            case FORBID:
-                printResultInfo(node.getVariable(), ResultKind.WRONG_APPLY_OPERATOR, mTrees, cut);
-                break;
-            case ALLOW_WITH_WARNING:
-                printResultInfo(node.getVariable(), ResultKind.APPLY_OPERATOR_WITH_WARNING, mTrees, cut);
-                break;
-            default:
-        }
+        checkOperandsPermission(node.getExpression(), node.getKind(), expr);
+        checkOperandsPermission(node.getVariable(), node.getKind(), var);
         return var;
     }
 
     @Override
     public String visitUnary(UnaryTree node, String aVoid) {
         String type = this.scan(node.getExpression(), aVoid);
-        Tree.Kind op = node.getKind();
-        PermissionPolicy permissionForExpr = isOperationAllow(op, type, processingEnv);
-        switch (permissionForExpr) {
-            case FORBID:
-                printResultInfo(node.getExpression(), ResultKind.WRONG_APPLY_OPERATOR, mTrees, cut);
-                break;
-            case ALLOW_WITH_WARNING:
-                printResultInfo(node.getExpression(), ResultKind.APPLY_OPERATOR_WITH_WARNING, mTrees, cut);
-                break;
-            default:
-        }
+        checkOperandsPermission(node.getExpression(), node.getKind(), type);
         return type;
     }
 
@@ -293,30 +277,11 @@ public class AnnotationValueTypeTreeScanner extends TreeScanner<String, String> 
     public String visitBinary(BinaryTree node, String aVoid) {
         String l = this.scan(node.getLeftOperand(), aVoid);
         String r = this.scan(node.getRightOperand(), aVoid);
-        Tree.Kind op = node.getKind();
         if (!isSubtype(l, r, processingEnv) && !isSubtype(r, l, processingEnv)) {
             printResultInfo(node, ResultKind.TYPE_MISMATCH_OPERAND, mTrees, cut);
         }
-        PermissionPolicy permissionForLeft = isOperationAllow(op, l, processingEnv);
-        PermissionPolicy permissionForRight = isOperationAllow(op, r, processingEnv);
-        switch (permissionForLeft) {
-            case FORBID:
-                printResultInfo(node.getLeftOperand(), ResultKind.WRONG_APPLY_OPERATOR, mTrees, cut);
-                break;
-            case ALLOW_WITH_WARNING:
-                printResultInfo(node.getLeftOperand(), ResultKind.APPLY_OPERATOR_WITH_WARNING, mTrees, cut);
-                break;
-            default:
-        }
-        switch (permissionForRight) {
-            case FORBID:
-                printResultInfo(node.getRightOperand(), ResultKind.WRONG_APPLY_OPERATOR, mTrees, cut);
-                break;
-            case ALLOW_WITH_WARNING:
-                printResultInfo(node.getRightOperand(), ResultKind.APPLY_OPERATOR_WITH_WARNING, mTrees, cut);
-                break;
-            default:
-        }
+        checkOperandsPermission(node.getLeftOperand(), node.getKind(), l);
+        checkOperandsPermission(node.getRightOperand(), node.getKind(), r);
         return generalizeTypes(l, r, processingEnv);
     }
 
